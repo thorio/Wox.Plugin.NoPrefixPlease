51b292fd6e2ed47fdc9fea9f1ef7485ec6f99ea0
thorio <thorio.git@gmail.com>
2021-05-26 21:21:14 +0200

    Initial commit

---
+++ Wox.Plugin.NoPrefixPlease/Configuration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Configuration;
+using System.Reflection;
+
+namespace Wox.Plugin.NoPrefixPlease
+{
+	static class Configuration
+	{
+		private static System.Configuration.Configuration _config;
+
+		static System.Configuration.Configuration Config
+		{
+			get
+			{
+				if (_config is null)
+				{
+					var configMap = new ExeConfigurationFileMap();
+					configMap.ExeConfigFilename = Assembly.GetAssembly(typeof(Configuration)).Location + ".config";
+					_config = ConfigurationManager.OpenMappedExeConfiguration(configMap, ConfigurationUserLevel.None);
+				}
+				return _config;
+			}
+		}
+
+		public static string GetKey(string key)
+		{
+			try
+			{
+				return Config.AppSettings.Settings[key].Value;
+			}
+			catch (NullReferenceException)
+			{
+				throw new ConfigurationErrorsException($"Missing configuration key '{key}'");
+			}
+		}
+	}
+}
---
+++ Wox.Plugin.NoPrefixPlease/Plugin.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Web;
+
+namespace Wox.Plugin.NoPrefixPlease
+{
+	class Plugin : IPlugin
+	{
+		public void Init(PluginInitContext context)
+		{
+		}
+
+		public List<Result> Query(Query query)
+		{
+			return new List<Result>() {
+				new Result() {
+					Action = context => Search(query.RawQuery),
+					Title = query.RawQuery,
+					Score = 0,
+					IcoPath = "icon.png",
+				}
+			};
+		}
+
+		private bool Search(string query)
+		{
+			var url = Configuration.GetKey("SearchUrl")
+				.Replace("{?}", HttpUtility.UrlEncode(query));
+			Process.Start(url);
+
+			return true;
+		}
+	}
+}
